b34f74c2889fbe1668342f94e48356b62b78bf0f
Caroline Farrior <cfarrior@utexas.edu>
2019-09-25 18:38:45 -0500

    editing axis labels, etc...

--- plotting_barephys.r
+++ plotting_barephys.r
@@ -17,14 +17,13 @@ library(RColorBrewer)
 x.lab = expression(paste(italic(x),", proportion of roots sent away"))
 n.lab = expression(paste(italic(n),", number of individuals"))
 fec.lab = expression(paste(italic(f),", fecundity (ind/yr)"))
-r.lab = expression(paste(italic(r),", fine root biomass (g)"))
-cx.lab = expression(atop(paste(italic(c),scriptstyle(x),", cost of sending roots"),"away (g/g)"))
+r.lab = expression(paste(italic(r),", fine-root biomass (g)"))
 nd.lab = expression(paste(italic(n),scriptstyle(d),", individual density (ind/d ",m^2,")"))
 mort.lab = expression(paste(italic(symbol(mu)),", mortality rate (1/yr)"))
 rd.lab = "fine root density, (g/dm2)"
 fd.lab = "plot fecundity (ind/dm2)"
 cxnot.lab = expression(paste(italic(c),scriptstyle(x),scriptstyle(0),", cost of sending roots away (g/g/dm)"))
-pcompo.lab = "competitive over-investment"
+pcompo.lab = "degree of overproliferation"
 Rlab = expression(paste(italic(R),", resource input rate (g",dm^2,"/yr)"))
 
 #some plotting defaults to be used
@@ -71,29 +70,26 @@ mort = 1
 	if(!savefig) X11(width=xplots*2.5,height=yplots*2.5)
 	par(mfrow=c(yplots,xplots),cex.lab=cxlab)
 	
-	plot(datac0$n,datac0$r,ylab="",xlab="",ylim=c(0,1.2),pch=19,col="gray",cex=pcx)
+	plot(datac0$n,datac0$r,ylab="",xlab="",ylim=c(0,1.5),pch=19,col="gray",cex=pcx)
 	lines(seq(2,10),seq(2,10)*0 + (R/cr-delta/uS),col="gray")
 	mtext("A",adj=0,line=2)
 	mtext(r.lab,side=2,line=2.5,cex=.8)
 	mtext(n.lab,side=1,line=2.5,cex=.8)
 	points(datac1$n,datac1$r,pch=19,col="black")
-	legend(4,.6,c("cx=0","cx=1.5","mean-field"),pch=c(19,19,NA),col=c("gray","black","gray"),lty=c(NA,NA,1),bty="n",cex=.75)
-	
-	plot(datac0$n,datac0$x,ylab="",xlab="",ylim=c(0,1.5),pch=19,col="gray",cex=pcx,yaxt="n")
-	axis(2,at = c(0,.5,1))
+
+	plot(datac0$n,datac0$x,ylab="",xlab="",ylim=c(0,1),pch=19,col="gray",cex=pcx)
 	lines(seq(2,10,by=0.01),1-1/seq(2,10,by=0.01),col="gray")
 	mtext("B",adj=0,line=2)
 	mtext(x.lab,side=2,line=2.5,cex=.8)
 	mtext(n.lab,side=1,line=2.5,cex=.8)
 	points(datac1$n,datac1$x,pch=19,col="black")
-	legend(1,1.65,c("cx=0","cx=1.5","mean-field"),pch=c(19,19,NA),col=c("gray","black","gray"),lty=c(NA,NA,1),bty="n",cex=.75)
 	
 	plot(datac0$n,datac0$fESS,ylab="",xlab="",pch=19,col="gray",cex=pcx,ylim=c(0,max(datac0$fESS)+.5))
 	lines(seq(2,10),seq(2,10)*0,col="gray")	
 	mtext("C",adj=0,line=2)
 	mtext(fec.lab,side=2,line=2.5,cex=.8)
 	mtext(n.lab,side=1,line=2.5,cex=.8)
-	points(datac1$n,datac1$fESS,pch=19,col="black")	
+	points(datac1$n,datac1$fESS,pch=19,col="black")
 	legend(4,2.2,c("cx=0","cx=1.5","mean-field"),pch=c(19,19,NA),col=c("gray","black","gray"),lty=c(NA,NA,1),bty="n",cex=.75)
 	
 	plot(datac0$n,(datac0$foptim-datac0$fESS)/datac0$foptim,ylim=c(0,1.05),ylab="",xlab="",pch=19,col="gray",cex=pcx)
@@ -102,8 +98,7 @@ mort = 1
 	mtext("degree of overproliferation",side=2,line=2.5,cex=.8)
 	mtext(n.lab,side=1,line=2.5,cex=.8)
 	points(datac1$n,datac1$pcompo,pch=19,col="black")
-	text(5,0.58,expression(paste("(",italic(f),scriptstyle(max),"-",italic(f),scriptstyle(ESS),")"/italic(f),scriptstyle(max),sep="")),cex=.75)
-	legend(2.5,.55,c("cx=0","cx=1.5","mean-field"),pch=c(19,19,NA),col=c("gray","black","gray"),lty=c(NA,NA,1),bty="n",cex=.75)
+	text(5,0.2,expression(paste("(",italic(f),scriptstyle(max),"-",italic(f),scriptstyle(ESS),")"/italic(f),scriptstyle(max),sep="")))
 	
 	if(savefig) dev.off()		
 }# end Figure 2
@@ -133,7 +128,7 @@ mort = 1
 		plot(cxdata$cx,cxdata[,column],type="n",ylab="",xlab="",ylim=ylim)
 		lines(cxdata$cx,cxdata[,column],pch=19,col="black",lwd=2)
 		mtext(y.lab,side=2,line=2.5,cex=.8)
-		mtext("Cost of sending roots \naway (cx)",side=1,line=3,cex=.8)
+		mtext("cx, cost of sending \nroots away",side=1,line=3,cex=.8)
 		mtext(letter,adj=0,line=2)
 
 		if(column==13) text(4.75,0.9,expression(paste("(",italic(f),scriptstyle(max),"-",italic(f),scriptstyle(ESS),")"/italic(f),scriptstyle(max),sep="")))
@@ -164,7 +159,7 @@ mort = 1
 	
 	aa$inv = round(aa$inv,4)
 
-	plot(aa$nrings,aa$nrings.i,type="n",xlab="Residents' #rings reached",ylab="Mutant's #rings reached",ylim=c(-.2,2.2),xlim=c(-.2,2.2),xaxt="n",yaxt="n")
+	plot(aa$nrings,aa$nrings.i,type="n",xlab="Resident's #rings reached",ylab="Mutant's #rings reached",ylim=c(-.2,2.2),xlim=c(-.2,2.2),xaxt="n",yaxt="n")
 	axis(1,c(0,1,2),c("0","1","2"))
 	axis(2,c(0,1,2),c("0","1","2"))
 	winners = aa[aa$inv>0,]
@@ -178,7 +173,7 @@ mort = 1
 
 }# end Figure 3
 
-{#Figure 4, Figure C2,C3,C4. Model 2. Predictions across environmental gradients, under various assumptions of the cost of moving roots through soil (cxnot) and leakage (delta). 
+{#Figure 4, Figure B2,B3,B4. Model 2. Predictions across environmental gradients, under various assumptions of the cost of moving roots through soil (cxnot) and leakage (delta). 
 
 	#fine = TRUE #use fine=FALSE when wanting to run faster and check coarse effects. 
 	
@@ -256,12 +251,12 @@ mort = 1
 			par(mfrow=c(yplots,xplots),cex.lab=1.1,oma=c(0,0,0,2),mar=c(4,4,4,2)+0.1)
 			
 			for(var in c("R","cx","del")){
-				if(var=="R") {data = Rvar; x =data$R; xlab=expression(paste(italic(R),", resourcee input (g ",dm^-2,yr^-1,")"));letter="A"}
-				if(var=="cx"){data = cxvar; x =data$cxnot; xlab=expression(paste(italic(c),scriptstyle(x),scriptstyle(0),", (",dm^-1, yr^-1,")"));letter="B"}
+				if(var=="R") {data = Rvar; x =data$R; xlab=expression(paste(italic(R),", resource input (g ",dm^-2,yr^-1,")"));letter="A"}
+				if(var=="cx"){data = cxvar; x =data$cxnot; xlab=expression(paste(italic(c),scriptstyle(x),scriptstyle(0)," (",dm^-1, yr^-1,")"));letter="B"}
 				if(var=="del"){data = delvar; x =data$delta; xlab=expression(paste(symbol(Delta),", leakage loss (",yr^-1,")"));letter="C"}
 					
 				plot(x,data$x,ylim=c(0,1.1),type="n",yaxt="n",xaxt="n",xlab="",ylab="")
-				if(var=="del") mtext(expression(paste(italic(r)," (g)")),side=4,line=2.5,cex=.75)
+				if(var=="del") mtext(expression(paste(italic(r))),side=4,line=2.5,cex=.75)
 				lines(x,data$x,col="red",lwd=1,lty=3)
 				lines(x,data$nd,lwd=lwd,lty=1)
 				axis(side=2,at=c(0,.5,1),cex=.5)
@@ -285,10 +280,10 @@ mort = 1
 			if(savefig) dev.off()
 		}
 	}
-}#end Figure 4, Figure C2,C3,C4
+}#end Figure 4, Figure B2,B3,B4
 
 
-{#Figure C5. Comparison of the ESS and population-level optimum of allocation to roots. Qualitative patterns are the same for r and r*nd as plotted. 
+{#Figure B5. Comparison of the ESS and population-level optimum of allocation to roots. Qualitative patterns are the same for r and r*nd as plotted. 
 	deltaD = 0.02; cxnotD = 1.2
 	fileadd = ""
 	if(fine) fileadd = "FINE"
@@ -307,19 +302,19 @@ mort = 1
 	par(mfrow=c(yplots,xplots),cex.lab=1.1,oma=c(0,0,0,2),mar=c(4,4,4,2)+0.1)
 	
 	for(var in c("R","cx","del")){
-		if(var=="R"){data = Rvar; x =data$R; xlab=expression(paste(italic(R),", rainfall (dm ",yr^-1,")"));letter="A"}
-		if(var=="cx"){data = cxvar; x =data$cxnot; xlab=expression(paste(italic(c),scriptstyle(x),scriptstyle(0),", root extension cost"));letter="B"}
+		if(var=="R"){data = Rvar; x =data$R; xlab=expression(paste(italic(R),", resource input (g ",dm^-2,yr^-1,")"));letter="A"}
+		if(var=="cx"){data = cxvar; x =data$cxnot; xlab=expression(paste(italic(c),scriptstyle(x),scriptstyle(0)," (",dm^-1, yr^-1,")"));letter="B"}
 		if(var=="del"){data = delvar; x =data$delta; xlab=expression(paste(symbol(Delta),", leakage loss (",yr^-1,")"));letter="C"}
 		
 		plot(x,data$r*data$nd,col="brown",pch=19,ylim=c(0,1.5),xlab="",type="n",ylab="",yaxt="n",xaxt="n")
 		mtext(xlab,side=1,cex=.75,line=3)
 		lines(x,data$r*data$nd,col=cols[2])
 		lines(x,data$roptim*data$nd,col=cols[3])
-		if(var=="R"||var=="cx") mtext(expression(paste("fine root biomass (g)")),side=2,line=2)
+		if(var=="R") mtext(expression(paste("fine root biomass (g)")),side=2,line=2)
 		axis(side=1,cex=.5)
 		axis(side=2,cex=.5)
 		
-		if(var=="R") legend(0.5,1.5,legend=c(expression(paste(italic(r),scriptstyle(ESS),"*",italic(n),scriptstyle(d),sep="")),expression(paste(italic(r),scriptstyle(OPTIM),"*",italic(n),scriptstyle(d),sep=""))),bty="n",col=c(cols[2],cols[3]),lwd=c(1,1),lty=c(1,1),cex=.75)
+		if(var=="R") legend(0.5,1.5,legend=c(expression(paste(italic(r),scriptstyle(ESS),"*",italic(n),scriptstyle(d),sep="")),expression(paste(italic(r),scriptstyle(OPTIM),"*",italic(n),scriptstyle(d),sep=""))),bty="n",col=c(cols[2],cols[3]),lwd=c(1,1),lty=c(1,1),cex=1)
 		
 	}	
 	if(savefig) dev.off()		
